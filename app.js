402aa4b57ae50e678cea8491887b86bdb55ec7af
Vladimer Mikava <mikava.vova@gmail.com>
2020-09-07 22:56:54 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-/*
-GAME FUNCTION:
-- Player must guess a number between a min and max
-- Player gets a certain amount of guesses
-- Notify player of guesses remaining
-- Notify the player of the correct answer if loose
-- Let player choose to play again
-*/
-
 // Game Values
 let min = 1,
     max = 10,
